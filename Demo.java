aca06aff316f666184a83a6b9ba0b48e67ecef08
PrasananTrainer03 <prassup.dotnet@gmail.com>
2022-07-27 09:39:33 +0530

    code

---
+++ Demo.java
@@ -0,0 +1,24 @@
+package com.java.day1;
+
+class Data {
+	
+	public void niki() {
+		System.out.println("Hi I am Niki...");
+	}
+	
+	private void vijay() {
+		System.out.println("Hi I am Vijay...");
+	}
+	
+	void prasanna() {
+		System.out.println("Hi I am Prasanna...");
+	}
+}
+
+public class Demo {
+	public static void main(String[] args) {
+		Data obj = new Data();
+		obj.niki();
+		obj.prasanna();
+	}
+}
